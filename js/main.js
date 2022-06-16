9b536e78ffeea0f01490f446e451e969d54397ae
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-06-16 14:58:45 +0200

    assign var N multiples of 3,5 inside var (i)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
+//* creioamo le variabili dei numeri lasciandola non definità
+let indexN;
+//* creioamo le variabili dei numeri multipli di 3 e 5 lasciandole non definità
+let nMultiple3
+let nMultiple5;
+
+
 // stampiamo in console i numeri progressivi da 1 a 100 
 
-for( let indexN = 1 ; indexN <= 100 ; indexN = indexN + 1){
-    console.log(indexN)
+for( let i = 1 ; i <= 100 ; i = i + 1){
+    //? assegniamo una var per i N° multipli di 3 e 5 all'interno della var (i)
+    let nMultiple3 = i % 3 == 0
+    let nMultiple5 = i % 5 == 0
+
+    console.log(nMultiple3)
+    console.log(nMultiple5)
+    console.log(i)
 }
